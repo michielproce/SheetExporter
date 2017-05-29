b1e0957f0e454448b0c450230e889508d8a1c7c8
michiel <mail@michielproce.nl>
2017-05-29 14:38:33 +0200

    Very basic colission check

--- Proce_SheetExporter.rb
+++ Proce_SheetExporter.rb
@@ -15,14 +15,15 @@ require 'sketchup.rb'
 require 'extensions.rb'
 require 'Proce_SheetExporter/functions.rb'
 require 'Proce_SheetExporter/sheet_properties_dialog.rb'
+require 'Proce_SheetExporter/check_collisions.rb'
 
 module Proce_SheetExporter
 
   sheet_properties_dialog = SheetPropertiesDialog.new
 
-  show_sheet_properties_command = UI::Command.new("Sheet properties") {
-    # show_ruby_panel()
+  #show_ruby_panel()
 
+  show_sheet_properties_command = UI::Command.new("Sheet properties") {
     sheet_properties_dialog.show
   }
 
@@ -33,8 +34,21 @@ module Proce_SheetExporter
   show_sheet_properties_command.status_bar_text = "Sheet properties"
   show_sheet_properties_command.menu_text = "Sheet properties"
 
+  check_collision_command = UI::Command.new("Check collision") {
+    CollisionChecker::check_collisions
+  }
+
+
+  check_collision_command.small_icon = "Proce_SheetExporter/icons/check_collision_small.png"
+  check_collision_command.large_icon = "Proce_SheetExporter/icons/check_collision_large.png"
+  check_collision_command.tooltip = "Check collisions"
+  check_collision_command.status_bar_text = "Check collisions"
+  check_collision_command.menu_text = "Check collisions"
+
+
   toolbar = UI::Toolbar.new "Sheet exporter"
   toolbar = toolbar.add_item show_sheet_properties_command
+  toolbar = toolbar.add_item check_collision_command
   toolbar.show
 
 
